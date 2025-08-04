db96131bd8415bb88271d6d3fa9b479073eeb7ad
samuelkdavis <samuelkdavis@gmail.com>
2025-08-04 21:03:52 +1200

    Add allow all CORS

--- api/Nosework/Nosework.Api/Program.cs
+++ api/Nosework/Nosework.Api/Program.cs
@@ -3,6 +3,8 @@ namespace Nosework.Api
 {
     public class Program
     {
+        private const string CorsAllowAll = "Allow all";
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -13,8 +15,16 @@ namespace Nosework.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: CorsAllowAll, policy =>
+                {
+                    policy.WithOrigins("*");
+                });
+            });
 
             var app = builder.Build();
+            app.UseCors(CorsAllowAll);
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
