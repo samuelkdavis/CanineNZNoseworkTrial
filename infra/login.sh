4515592da6868517d8011b2ebe8e56bd13a3fd1e
samuelkdavis <samuelkdavis@gmail.com>
2025-09-01 21:46:20 +1200

    Get AWS Login script working in CLI
    * Previously it'd set the developer profile within the credentials file, but the profile had to be explicitly referenced

--- infra/login.sh
+++ infra/login.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# call with `. ./infra/login.sh <MFA_TOKEN>`. If you leave off the first dot the env vars wont be set in your current shell.
+
 mfa_token=$1
 CREDENTIALS=$(aws sts get-session-token --serial-number "arn:aws:iam::956470542728:mfa/ProtonPass" --output json --token-code $mfa_token)
 
@@ -18,3 +20,6 @@ PROFILE_NAME="developer"
 aws configure set aws_access_key_id "$ACCESS_KEY_ID" --profile "$PROFILE_NAME"
 aws configure set aws_secret_access_key "$SECRET_ACCESS_KEY" --profile "$PROFILE_NAME"
 aws configure set aws_session_token "$SESSION_TOKEN" --profile "$PROFILE_NAME"
+
+export AWS_PROFILE=$PROFILE_NAME
+echo "AWS_PROFILE set to $PROFILE_NAME"
